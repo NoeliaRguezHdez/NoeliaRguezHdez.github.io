9e4a73740a36b4d009a2d9a2b4ca27bab5602fb4
NoeliaRguezHdez <alu0100595420@ull.edu.es>
2021-12-07 11:51:59 +0000

    new-deploy

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Bienvenido!!",
+        "excerpt":"Descripción El Máster Interuniversitario en Formación del Profesorado de Educación Secundaria Obligatoria y Bachillerato, Formación Profesional y Enseñanza de Idiomas por las Universidades de La Laguna y Las Palmas de Gran Canaria, habilita para el ejercicio de actividades profesionales, según la Orden EDU/3498/2011, de 16 de diciembre, por la que...","categories": ["inicio"],
+        "tags": [],
+        "url": "/inicio/inibienvenido",
+        "teaser": null
+      },{
+        "title": "Aprendizaje y eñseñanza",
+        "excerpt":"Individual El aprendizaje individual es aquel que desarrolla, en mayor medida, el aprendizaje aislado de los alumnos. Es el utilizado en el sistema clásico de la educación en la que el profesor desempeña un rol expositor y los alumnos un rol de escucha y atención a sus palabras. La disposición...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/aprendizaje",
+        "teaser": null
+      },{
+        "title": "Dolphin post",
+        "excerpt":"Personal post   Dolphin post     Notas de clase  Somos 20 alumnos   El resultado de chuchu es 4   Probando estructuras   En breve es fiiindeee!!   Frutitas           Me gusta el melón            Me gusta el coco            Me gusta el higo       Mi usuario es AET developing.   ","categories": ["personal"],
+        "tags": [],
+        "url": "/personal/dolphin",
+        "teaser": null
+      },{
+        "title": "Math post",
+        "excerpt":"   Probando blog  \\(f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\)   \\(mean = \\frac{\\displaystyle\\sum_{i=1}^{n} x_{i}}{n}\\)                                  First name                Last name                Age                Location                                  John                Doe                35                United States                                 Jane                Doe                29                France                                 Jack                Hill                25                Australia                ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/clase",
+        "teaser": null
+      },{
+        "title": "Datos de contacto",
+        "excerpt":"Universidad de La Laguna   Avda. Astrofísico Francisco Sánchez, SN. Edificio Calabaza AN.2D Apdo. 456 38200 San Cristóbal de La Laguna master.profesorado@ull.es     ","categories": ["contacto"],
+        "tags": [],
+        "url": "/contacto/naet",
+        "teaser": null
+      },{
+        "title": "Objetivos",
+        "excerpt":"Principales Informar y asesorar a las familias acerca del proceso de enseñanza y aprendizaje y sobre la orientación personal, académica y profesional de sus hijos Conocer la normativa y organización institucional del sistema educativo y modelos de mejora de la calidad con aplicación a los centros de enseñanza Adquirir estrategias...","categories": ["inicio"],
+        "tags": [],
+        "url": "/inicio/iniobj",
+        "teaser": null
+      },{
+        "title": "Sociología",
+        "excerpt":"El funcionalismo Hegemónico en los años 50-60. Analiza las relaciones entre sistema educativo y necesidades del sistema social (funciones de la educación). El sistema social tiene tendencia a la estabilidad Cada elemento del sistema tiene una función . La cultura es elemento central en el mantenimiento del orden social. Teoría...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/d1",
+        "teaser": null
+      }]
